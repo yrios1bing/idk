31366886825320f6ae1ffeb25951628d3217357b
yrios1bing <116851789+yrios1bing@users.noreply.github.com>
2022-10-27 19:48:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+#Part A
+weeks = 16
+
+#ask for user input for total_tuition and classes_per_week (don't forget type conversions)
+
+
+#calculate the cost_per_class using the provided 'weeks' variable and the data from the user\
+
+
+#print your result
